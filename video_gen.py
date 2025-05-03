17b080bfa16d480a4c0268bd8b13891f6f087ab3
Srijan Purkait <srijanpurkait2011@gmail.com>
2025-05-03 11:09:37 +0530

    Migrated from moviepy to pyav
    6x video generating speed gained

--- video_gen.py
+++ video_gen.py
@@ -1,23 +1,115 @@
-import moviepy as mov
+import av
+import time
+from PIL import Image, ImageFont, ImageDraw
 import srt
+import threading
 
-def generate_video(background_video_path, thumbnail_path, font_path, audio_path, subtitles_path, video_path):
-    subtitles_list = []
+mux_lock = threading.Lock()
+
+from concurrent.futures import ThreadPoolExecutor
+
+def wrap_text(text:str, font_size, width):
+    text_list = text.split(' ')
+    line = text_list[0]
+    final_str = ''
+    text_list.pop(0)
+    
+    for x in text_list:
+        if len(line + ' ' + x) * font_size * 0.6 < width:
+            line += ' ' + x
+        else:
+            final_str += '\n' + line
+            line = x
+
+    final_str += '\n' + line
+
+    return final_str[1:]
+
+def process_frame(frame_idx, frame, thumbnail, subtitles_image_data, video_stream, output):
+    frame = frame.to_image()
+    frame = frame.crop(((frame.width - 608) // 2, 0, (frame.width + 608) // 2, 1080))
+
+    if frame_idx < 20 * 5:
+        frame.paste(thumbnail, (0, 300))
+
+    if subtitles_image_data and frame_idx > subtitles_image_data[0][1]:
+        frame = Image.alpha_composite(frame.convert("RGBA"), subtitles_image_data[0][0])
+
+    if subtitles_image_data and frame_idx > subtitles_image_data[0][2]:
+        subtitles_image_data.pop(0)
+
+    out_frames = av.VideoFrame.from_image(frame)
+    with mux_lock:
+        for packet in video_stream.encode(out_frames):
+            output.mux(packet)
+
+    return frame
+
+def generate_video(output_path, font_path, audio_path, background_video_path, thumbnail_path, subtitles_path): 
+    start_time = time.time()
+    print(f"Starting: {time.time() - start_time}")
+
+    background_video = av.open(background_video_path)
+    narrator_audio = av.open(audio_path)
+
+    font = ImageFont.truetype(font_path, size=28)
+
+    frame_skip = background_video.streams.video[0].average_rate // 20
+
+    output = av.open(output_path, 'w')
+    video_stream = output.add_stream('h264', 20, options={
+        "crf": '24',
+        "preset": 'ultrafast',
+        'tune': 'fastdecode',
+        'movflags': '+faststart',
+    })
+
+    video_stream.height = 1080
+    video_stream.width = 608
+    video_stream.pix_fmt = 'yuv420p'
+
+    in_audio_stream = narrator_audio.streams.audio[0]
+    out_audio_stream = output.add_stream('mp3', rate=in_audio_stream.rate)
+
+    video_frames = background_video.decode()
+
+    thumbnail = Image.open(thumbnail_path)
+    thumbnail = thumbnail.resize((608, int(thumbnail.height * (608 / thumbnail.width))))
+
+    video_duration = narrator_audio.duration
+
+    subtitles_image_data = []
     with open(subtitles_path) as f:
         for sub in srt.parse(f.read()):
-            subtitles_list.append(mov.TextClip(text=sub.content, font=font_path, font_size=32, size=(600,300), color='#ffffff', method='caption', duration=(sub.end - sub.start).total_seconds()))
+            img = Image.new('RGBA', (608, 1080), (0,0,0,0))
+            img_draw = ImageDraw.Draw(img)
 
-    subtitles = mov.concatenate_videoclips(subtitles_list, method='compose').with_position("top")
+            img_draw.multiline_text((304,700), wrap_text(sub.content, 28, 608), (255, 255, 255, 255), font, anchor='mm', align='center')
 
-    thumbnail_image = mov.ImageClip(thumbnail_path).with_position("center").resized(width=608).with_duration(5)
-    narrator_audio = mov.AudioFileClip(audio_path)
-    background_video = mov.VideoFileClip(background_video_path).subclipped(0, narrator_audio.duration)
-    background_video = background_video.cropped((background_video.w - 608) // 2, 0, ((background_video.w - 608) // 2) + 608, 1080)
+            subtitles_image_data.append([img, sub.start.total_seconds() * 20, sub.end.total_seconds() * 20])
 
-    full_video = mov.CompositeVideoClip([background_video, thumbnail_image, subtitles]).subclipped(0, narrator_audio.duration)
-    full_video.audio = mov.CompositeAudioClip([narrator_audio])
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        for i, frame in enumerate(video_frames):
+            if i % frame_skip != 0: continue
+            i = i // frame_skip
+            if i > 20 * video_duration: break
 
-    full_video.write_videofile(video_path, codec="libx264", audio_codec="aac", fps=20, threads=16, preset='ultrafast', ffmpeg_params=['-crf', '30'])
+            if i % 20 == 0: print(f"{i // 20} sec: {time.time()-start_time}")
 
-if __name__ == "__main__":
-    generate_video("files/asset/minecraft_parkour_1.mp4", f"files\\thumbnail\\1k11fik.png", "files\\font\\futur.ttf", f"files\\audio\\1k11fik.mp3", f"files\\subtitles\\1k11fik.srt", f"results\\1k11fik.mp4")
+            executor.submit(process_frame, i, frame, thumbnail, subtitles_image_data, video_stream, output)
+
+        for packet in video_stream.encode():
+            output.mux(packet)
+
+    for packet in narrator_audio.demux(in_audio_stream):
+        if packet.dts is None:
+            continue
+        packet.stream = out_audio_stream
+        output.mux(packet)
+    
+    output.close()
+
+    time_taken = time.time() - start_time
+    print(f"Time taken: {time_taken}")
+    
+    return video_duration
--- youtube_uploader.py
+++ youtube_uploader.py
@@ -1,16 +1,19 @@
 from youtube_upload.client import YoutubeUploader
 
-def upload(video_path, secrets_file_path, title, description):
+def upload(video_path, secrets_file_path, title, description, video_duration):
     uploader = YoutubeUploader(secrets_file_path= secrets_file_path)
     uploader.authenticate()
 
     options = {
         "title": title,
         "description": description,
-        "tags": ["shorts", "reddit", "funny", "storytime"],
+        "tags": ["reddit", "storytime"],
         "categoryId": "22",
         "privacyStatus": "public",
-        "kids": False } 
+        "kids": False }
+
+    if video_duration < 180:
+        options["tags"] = ["shorts", "reddit", "storytime"]
     
     uploader.upload(video_path, options)
 
