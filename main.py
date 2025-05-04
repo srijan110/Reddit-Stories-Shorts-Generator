a0603c5a10616c852aaa43c4847d2c2536144530
Srijan Purkait <srijanpurkait2011@gmail.com>
2025-05-04 06:16:13 +0530

    Early Auththentication
    Earlier we need to authenticate every time and after video generation now only once a day and at first authentication is done

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import thumbnail
 from subtitles_gen import generate_subtitles
 from video_gen import generate_video
 from random import choice
-from youtube_uploader import upload
+from youtube_uploader import upload, authenticate
+
+authenticate("files\\client_secret.json", 'files\\oauth.json')
 
 subreddits = ["offmychest", "confession", "TrueOffMyChest", "relationships", "shortscarystories",
     "pettyrevenge", "TodayILearned","AmITheAsshole"]
@@ -23,4 +25,4 @@ generate_subtitles(f"files\\audio\\{id}.mp3", f"files\\subtitles\\{id}.srt")
 
 video_duration = generate_video(f"results\\{id}.mp4", "files\\font\\futur.ttf", f"files\\audio\\{id}.mp3", "files/asset/minecraft_parkour_1.mp4", f"files\\thumbnail\\{id}.png", f"files\\subtitles\\{id}.srt")
 
-upload(f"results\\{id}.mp4", "files\\client_secret.json", title, f"{title} - {author}", video_duration)
+upload(f"results\\{id}.mp4", title, f"{title} - {author}", video_duration)
--- youtube_uploader.py
+++ youtube_uploader.py
@@ -1,8 +1,27 @@
 from youtube_upload.client import YoutubeUploader
+import json
+from datetime import datetime
+import os
 
-def upload(video_path, secrets_file_path, title, description, video_duration):
+oauth_path = ''
+
+def authenticate(secrets_file_path, oauth_file_path):
+    global oauth_path
+
+    oauth_path = oauth_file_path
+    with open(oauth_path) as f:
+        doc = json.load(f)
+
+    if datetime.strptime(doc['token_expiry'], r"%Y-%m-%dT%H:%M:%SZ") > datetime.now():
+        os.remove(oauth_path)
+
     uploader = YoutubeUploader(secrets_file_path= secrets_file_path)
-    uploader.authenticate()
+    uploader.authenticate(oauth_path)
+
+
+def upload(video_path, title, description, video_duration):
+    with open(oauth_path) as f:
+        oauth = f.read()
 
     options = {
         "title": title,
@@ -10,11 +29,18 @@ def upload(video_path, secrets_file_path, title, description, video_duration):
         "tags": ["reddit", "storytime"],
         "categoryId": "22",
         "privacyStatus": "public",
-        "kids": False }
+        "kids": False 
+    }
 
     if video_duration < 180:
         options["tags"] = ["shorts", "reddit", "storytime"]
     
     uploader.upload(video_path, options)
+    del(uploader)
 
-    uploader.close()
+    with open(oauth_path) as f:
+        f.write(oauth)
+
+if __name__ == "__main__":
+    authenticate("files\\client_secret.json", 'files\\oauth.json')
+    upload('results/e5k3z2.mp4', 'Test Video', 'Test Video', 300)
